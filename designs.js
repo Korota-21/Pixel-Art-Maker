4e6d52df6efda46097a5e14d05ea87d70c10d66b
korota <koroko123430@gmail.com>
2023-01-13 10:59:53 +0300

    تذطر لون

--- designs.js
+++ designs.js
@@ -1,27 +1,46 @@
 // Select color input
-var color = document.getElementById("colorPicker");
+const color = document.getElementById("colorPicker");
 // Select size input
-var Height = document.getElementById("inputHeight");
-var Width = document.getElementById("inputWidth");
-
+const Height = document.getElementById("inputHeight");
+const Width = document.getElementById("inputWidth");
+const lastColorsArr = []
+const lastColorsGrid = document.getElementById("lastColors");
 // When size is submitted by the user, call makeGrid()
-var myGrid = document.getElementById("pixelCanvas");
+const myGrid = document.getElementById("pixelCanvas");
 
-var form = document.getElementById("sizePicker")
+const form = document.getElementById("sizePicker")
 form.addEventListener("submit", handleForm);
-
-
 function handleForm(event) {
     makeGrid();
     event.preventDefault();
 }
 
+color.addEventListener('change', () => {
+    if (lastColorsArr.length >= 6)
+        lastColorsArr.shift();
+    lastColorsArr.push(color.value)
+    console.log(lastColorsArr)
+    let grid = "";
+    grid += "<tr>";
+    for (let i = 0; i < lastColorsArr.length; i++) {
+        grid += `<td style="background-color:${lastColorsArr[i]}" id="${lastColorsArr[i]}" ></td>`;
+    }
+    grid += "</tr>";
+    lastColorsGrid.innerHTML = grid;
+    lastColorsGrid.removeEventListener('click', changeColor); // عشان مايتكرر
+    lastColorsGrid.addEventListener('click', changeColor);
+})
+
+function changeColor(evt) {
+    color.value = evt.target.id;
+    console.log(color.value);
+}
+
 function makeGrid() {
-    // Your code goes here!
-    var grid = "";
-    for (var row = 1; row <= Height.value; row++) {
+    let grid = "";
+    for (let row = 1; row <= Height.value; row++) {
         grid += "<tr>";
-        for (var col = 1; col <= Width.value; col++) {
+        for (let col = 1; col <= Width.value; col++) {
             grid += "<td></td>";
         }
         grid += "</tr>";

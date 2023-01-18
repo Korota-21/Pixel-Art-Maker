8d58e994b906264b447078df59166bb8280bbc48
korota <koroko123430@gmail.com>
2023-01-18 10:07:53 +0300

    handle errors

--- ts-js/convertToImage.js
+++ ts-js/convertToImage.js
@@ -1,12 +1,19 @@
 "use strict";
 function convertToImage() {
     let resultDiv = document.getElementById("result");
+    if (myGrid.innerHTML == '') {
+        resultDiv.innerHTML = 'there are no drawing';
+        return;
+    }
     // @ts-ignore
     html2canvas(myGrid, {
         onrendered: function (canvas) {
-            var img = canvas.toDataURL("image/png");
-            resultDiv.innerHTML = '<a download="myImage.jpeg" href="' + img + '">تحميل</a>';
-            resultDiv.innerHTML += '<img src="' + img + '"/>';
+            resultDiv.innerHTML = '';
+            const img = canvas.toDataURL("image/png");
+            const imgElm = document.createElement('img');
+            imgElm.src = img;
+            resultDiv.appendChild(imgElm);
+            resultDiv.innerHTML += '<br><a download="myImage.jpeg" href="' + img + '" class="button"><button>Click me !</button></a>';
         }
     });
 }
--- ts/convertToImage.ts
+++ ts/convertToImage.ts
@@ -1,11 +1,18 @@
 function convertToImage() {
     let resultDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("result");
+    if (myGrid.innerHTML == '') { 
+        resultDiv.innerHTML = 'there are no drawing'; 
+        return 
+    }
     // @ts-ignore
     html2canvas(myGrid, {
         onrendered: function (canvas: any) {
-            var img = canvas.toDataURL("image/png");
-            resultDiv.innerHTML = '<a download="myImage.jpeg" href="' + img + '">تحميل</a>';
-            resultDiv.innerHTML += '<img src="' + img + '"/>';
+            resultDiv.innerHTML = ''
+            const img = canvas.toDataURL("image/png");
+            const imgElm = document.createElement('img');
+            imgElm.src = img
+            resultDiv.appendChild(imgElm)
+            resultDiv.innerHTML += '<br><a download="myImage.jpeg" href="' + img + '" class="button"><button>Click me !</button></a>';
         }
     }
     );
